823a2cbc7d5ddc39c7934329b1eebb54f3601523
Sovak <Sovak007@gmail.com>
2013-11-11 21:08:51 +0100

    Added forgotten files for last my last commit
    Signed-off-by: Sovak <Sovak007@gmail.com>

---
+++ SpellWork/DBC/Structures/SpellEffectScalingEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace SpellWork.DBC.Structures
+{
+    public class SpellEffectScalingEntry
+    {
+        public uint Id;
+        public float Multiplier;
+        public float RandomPointsMultiplier;
+        public float OtherMultiplier;
+        public uint SpellEffectId;
+    }
+}
---
+++ SpellWork/DBC/Structures/SpellMiscEntry.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using DBFilesClient.NET;
+using System.Text;
+
+namespace SpellWork.DBC.Structures
+{
+    public class SpellMiscEntry
+    {
+        public uint Id;
+        public uint SpellId;
+        public uint Attributes;
+        public uint AttributesEx;
+        public uint AttributesEx2;
+        public uint AttributesEx3;
+        public uint AttributesEx4;
+        public uint AttributesEx5;
+        public uint AttributesEx6;
+        public uint AttributesEx7;
+        public uint AttributesEx8;
+        public uint AttributesEx9;
+        public uint AttributesEx10;
+        public uint AttributesEx11;
+        public uint AttributesEx12;
+        public uint CastingTimeIndex;
+        public uint DurationIndex;
+        public uint RangeIndex;
+        public float Speed;
+        [StoragePresence(StoragePresenceOption.Include, ArraySize = 2)]
+        public uint[] SpellVisual;
+        public uint SpellIconID;
+        public uint ActiveIconID;
+        public uint SchoolMask;
+    }
+}
